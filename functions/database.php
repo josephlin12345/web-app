19bd2139364caf5afd44bbe3fd8a5229c22b4322
LinJoseph <josephlin12345@gmail.com>
2021-05-30 09:14:27 +0800

    update db class

--- functions/database.php
+++ functions/database.php
@@ -5,21 +5,17 @@
     private $dbname = 'project';
     private $username = 'root';
     private $password = '';
-    private $conn;
 
     // connect to db
     public function connect() {
-      $this->conn = null;
-
       try {
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbname, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->conn;
       }
       catch(PDOException $e) {
         die('Database Connection Error: ' . $e->getMessage());
       }
-
-      return $this->conn;
     }
   }
 
