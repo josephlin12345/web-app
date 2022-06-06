6524aeef83425bd40d46fa050958cbe294d4c284
linlin <49875534+josephlin12345@users.noreply.github.com>
2022-06-06 18:28:20 +0800

    Update about.php

--- about.php
+++ about.php
@@ -7,7 +7,6 @@
 <div class="title"><?php echo $text[$title][$lang]; ?></div>
 <div class="form">
   <p><?php echo $text['owner'][$lang]; ?> : I3B24 林浚喆</p>
-  <p><?php echo $text['collaborator'][$lang]; ?> : I3B59 許家豪、I3B09 沈大中</p>
 </div>
 
 <?php require 'templates/footer.php'; ?>
